61e47e02e0d46b66a3bf31fb7aa1b784a0b7df5d
hpjack666 <543261544@qq.com>
2024-12-03 16:03:46 +0800

    Site updated: 2024-12-03 16:03:45

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1732686860412";
+const VERSION = "1732872061208";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
